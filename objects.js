913ae662983d6ec98d938c85aad688e721d75d10
Ayush Gehlot <ayush_gehlot@hotmail.com>
2019-02-10 20:42:06 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 var playlist = {"joey bada$$":"Christ conscious" }
 
 
@@ -9,4 +10,17 @@ function updatePlaylist (playlist, artist, song){
 function removeFromPlaylist(playlist, artist){
   delete playlist[artist]; 
   return playlist
+=======
+var playlist = {joey: "christ conscious"}
+
+/*
+function updatePlaylist (obj, key, value){
+  return Object.assign({}, obj, {[key]:value}); 
+}
+
+*/
+
+function updatePlaylist (obj, key, value){
+  obj.key = value;
+>>>>>>> 6e348536b920f805870a12c8dbb20af680d4ca05
 }
